2475fcfd79d5ab1cd65992074cc0bd882f888fc6
RSO77 <Ostapchenko.roma@gmail.com>
2022-05-24 13:18:40 +0300

    Final fixed

--- Router.js
+++ Router.js
@@ -3,15 +3,15 @@ import UserController from './UserController.js';
 
 const router = new Router();
 
-router.post('/user', UserController.create)
+router.post('/users', UserController.create)
 router.get('/users', UserController.getAll)
-router.get('/user/:id', UserController.getOne)
-router.put('/user', UserController.update)
-router.delete('/user/:id', UserController.delete)
+router.get('/users/:id', UserController.getOne)
+router.put('/users', UserController.update)
+router.delete('/users/:id', UserController.delete)
 
-router.put('/user/add/:userId/:newUserId', UserController.addUserFriends)
-router.put('/user/accept/:userId/:newUserId', UserController.userAcceptFriend)
-router.put('/user/remove/:userId/:newUserId', UserController.removeUserFriends)
+router.put('/users/add/:userId/:newUserId', UserController.addUserFriends)
+router.put('/users/accept/:userId/:newUserId', UserController.userAcceptFriend)
+router.put('/users/remove/:userId/:newUserId', UserController.removeUserFriends)
 router.get('/users/showFriends/:userId', UserController.showFriends)
 router.get('/users/showRequest/:userId', UserController.showUserRequest)
 
